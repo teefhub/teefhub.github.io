b47c223cb6524d876bcb2acf1be815e0b760feb6
Tiffany Lee <46877769+teefeelee@users.noreply.github.com>
2019-04-05 14:25:04 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 $('#menu-button').on('click', function() {
   $(document.body).toggleClass('menu-open');
 });
-
-$(document).ready(function(){
- 				$("#hide").click(function(){
-			    $("p").hide();
-			  });
-			  $("#show").click(function(){
-			    $("p").show();
-			  });
-			});
