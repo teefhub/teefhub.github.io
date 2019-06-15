b198340a2ac6a92b1b41dc52cd7080000e5dc2ca
teefeelee <46877769+teefeelee@users.noreply.github.com>
2019-06-15 21:02:23 +1000

    update

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-$(".menu-toggle").on('click', function() {
-  $(this).toggleClass("on");
-  $('.menu-section').toggleClass("on");
-  $("nav ul").toggleClass('hidden');
-});
-};
