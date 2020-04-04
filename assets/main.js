a2189e52a9c939ab3f4f7e6246dcd0f199d7486c
tiffylee <46877769+tiffylee@users.noreply.github.com>
2020-04-04 15:54:58 +1100

    r

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+
+    window.onload = function() { document.body.classList.remove('is-preload'); }
+    window.ontouchmove = function() { return false; }
+    window.onorientationchange = function() { document.body.scrollTop = 0; }
